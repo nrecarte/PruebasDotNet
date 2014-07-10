5eaae6a87ffd67d9398365da2f77978b2683be6a
Noel Recarte <noelrecarte@gmail.com>
2014-07-10 11:21:19 -0600

    REP

---
+++ PruebasDotNet/SegConRep/Controllers/ActorController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SegConRep;
+
+namespace SegConRep.Controllers
+{
+    public class ActorController : Controller
+    {
+        private sakilaEntities db = new sakilaEntities();
+
+        // GET: /Actor/
+        public ActionResult Index()
+        {
+            return View(db.actor.ToList());
+        }
+
+        // GET: /Actor/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            actor actor = db.actor.Find(id);
+            if (actor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(actor);
+        }
+
+        // GET: /Actor/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Actor/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="actor_id,first_name,last_name,last_update")] actor actor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.actor.Add(actor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(actor);
+        }
+
+        // GET: /Actor/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            actor actor = db.actor.Find(id);
+            if (actor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(actor);
+        }
+
+        // POST: /Actor/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="actor_id,first_name,last_name,last_update")] actor actor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(actor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(actor);
+        }
+
+        // GET: /Actor/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            actor actor = db.actor.Find(id);
+            if (actor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(actor);
+        }
+
+        // POST: /Actor/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            actor actor = db.actor.Find(id);
+            db.actor.Remove(actor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ PruebasDotNet/SegConRep/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SegConRep.MvcApplication" Language="C#" %>
