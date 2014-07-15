17e47d314bc49550bf6b0f055096ea42458033b3
Noel Recarte <noelrecarte@gmail.com>
2014-07-15 17:00:43 -0600

    15/7/2014

---
+++ PruebasDotNet/SegConRep/Clases/SegconHelpers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Prueba.Clases
+{
+    public class SegconHelpers
+    {
+        public static string Url(HtmlHelper helper, string target, string text)
+        {
+            return String.Format("<label for='{0}'>{1}</label>", target, text);
+
+        }
+    }
+}
